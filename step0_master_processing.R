805d4745e8eaf6644846a2e207579cba4a778feb
nora-lvr <livestockvets@gmail.com>
2025-02-14 10:39:35 -0600

    modified instructions

--- step0_master_processing.R
+++ step0_master_processing.R
@@ -7,32 +7,21 @@ library(quarto)
 
 start<-now()
 
-#***IMPORTANT*** make sure you have opened the 'step1_ReadInDate.Rmd' file and completed the set up 
-#instructions before running this master processing script
-
-
 ### Step 1 Read in data----------
+#***Modify This Step to Include Correctly Parse Location and Other custom functions***
 source('step1_read_in_data.R')
 
-
-
-
-
-#add custom event mapping
-#add custom remark parsing
-#add custom protocol parsing
-
-
 ### Step 2 Intermediate Files----------------------
+#***Modify This Step to Include the Events/Disease of Interest***
 source('step2_create_intermediate_files.R')
 
+### Step 3 Report Templates------------------------
+#add basic report templates
+quarto::quarto_render('step3_report_templates.qmd')
+#cohort disease incidence (Location, Lactation, Breed, etc)
+#timing of disease (DIM (or Age) and calendar time distributions, Kaplan Meier)
+#perfomrance and disease (milk, gain, repro)
 
 end<-now()
 
 end-start
-
-### Step 3 Report Templates------------------------
-#add basic report templates
-#cohort disease incidence (Location, Lactation, Breed, etc)
-#timing of disease (DIM (or Age) and calendar time distributions, Kaplan Meier)
-#perfomrance and disease (milk, gain, repro)
